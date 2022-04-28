46b2c64ade74c0789cac90360d1a3d7635d276f0
unknown <xpetrik2@ad.fi.muni.cz>
2022-04-28 16:48:10 +0200

    commit-02

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
-import yaml
-import subprocess
 import hashlib
+import subprocess
+import yaml
 import subprocess
 import flask
 
 
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=file)
+    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)
 
 
@@ -27,7 +27,7 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
     http = urllib.PoolManager()
-    r = http.request('GET', url)
+    my_r = http.request('GET', url)
     return r.data
 
 
